4582ec3eed5cf680154cb685ea91691e90931bc0
Albert <zhang651651@hotmail.com>
2018-01-18 16:01:47 +0800

    2

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'duxin' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  pod 'AFNetworking', '~> 3.0'
+  pod 'SDWebImage', '~>3.8'
+  pod 'SDCycleScrollView','~> 1.64'
+  pod 'DTCoreText'
+  pod 'MBProgressHUD'
+  pod 'IQKeyboardManager'
+
+  # Pods for duxin
+
+  target 'duxinTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'duxinUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ duxinTests/duxinTests.m
@@ -0,0 +1,39 @@
+//
+//  duxinTests.m
+//  duxinTests
+//
+//  Created by 37duxin on 18/01/2018.
+//  Copyright © 2018 37duxin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface duxinTests : XCTestCase
+
+@end
+
+@implementation duxinTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
